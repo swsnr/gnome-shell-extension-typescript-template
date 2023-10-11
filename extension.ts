7506ff9ff53348a625485f2dbb6b7e48844cc403
Sebastian Wiesner <sebastian@swsnr.de>
2023-10-11 20:27:29 +0200

    Use an import to assert that types are working

--- extension.ts
+++ extension.ts
@@ -2,4 +2,6 @@
 // License, v. 2.0.If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-console.log("HELLO WORLD");
+import GLib from "gi://GLib?version=2.0";
+
+console.log("HELLO WORLD", GLib.get_user_config_dir());
